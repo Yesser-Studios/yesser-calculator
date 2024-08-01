a4b006b1e902519c99b6be9df5b3bbe59de52164
yesseruser <yesseruseryt@gmail.com>
2024-08-01 22:46:16 +0200

    Fix symbol swap between division and subtracton

--- CalculatorApp/Operations/Subtraction.cs
+++ CalculatorApp/Operations/Subtraction.cs
@@ -5,5 +5,5 @@ public class Subtraction : IOperation
     public double Execute(double number1, double number2)
         => number1 - number2;
     
-    public string Symbol => "/";
+    public string Symbol => "-";
 }
